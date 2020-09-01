618aac426c671055ca4ed1a0063d24b30de25670
Jin_ <simonjin0527@gmail.com>
2020-09-01 15:12:45 +0900

    add views

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
+
+const app = express();
+
+app.use(helmet());
+app.set("view engine", "pug");
+//**** Middleware ****//
+// app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan("dev"));
+app.use(localsMiddleware);
+
+//**** Router ****//
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+
+export default app;
---
+++ routers/userRouter.js
@@ -0,0 +1,16 @@
+import express from "express";
+import routes from "../routes";
+import {users, userDetail, editProfile, changePassword} from "../controllers/userController";
+
+const userRouter = express.Router();
+
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+
+export default userRouter;
+
+// userRouter.get("/", (req, res) => res.send('user index'));
+// userRouter.get("/edit", (req, res) => res.send('user edit'));
+// userRouter.get("/password", (req, res) => res.send('user password'));
+
